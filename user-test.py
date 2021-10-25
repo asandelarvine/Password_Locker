9def7a116f5417bcb0fe7ee117883cf209eff30d
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-25 08:48:38 +0300

    test_save_user,test_save__users,test_delete_user and test_find_user methods implemented

--- user-test.py
+++ user-test.py
@@ -26,3 +26,46 @@ class TestUser(unittest.TestCase):
             """
             self.assertEqual(self.new_user.username, "Larvine")
             self.assertEqual(self.new_user.password, "asande@123")
+
+        def test_save_user(self):
+            """
+            test_save_user confirms if the users details are saved into the user list
+            """
+            self.new_user.save_user()
+            self.assertEqual(len(User.user_list),1)
+
+        def test_save_multiple_users(self):
+            """
+            checks if one can store more than one user 
+            """
+            self.new_user.save_user()
+            test_user = User("test", "password")
+            test_user.save_user()
+            self.assertEqual(len(User.user_list), 2)
+
+        def test_delete_user(self):
+            """
+            checks whether one can delete a user account
+            """
+            self.new_user.save_user()
+            test_user = User("test","password")
+            test_user.save_user()
+            self.new_user.delete_user()
+            self.assertEqual(len(User.user_list), 1)
+
+        def test_find_user(self):
+            """
+            checks if one can find a user by username and display its info
+            """
+            self.new_user.save_user()
+            test_user = User("test","password")
+            test_user.save_user()
+            found_user = User.find_user("Larvine")
+            self.assertEqual(found_user.username, self.new_user.username)
+
+    if __name__ == '__main__':
+        unittest.main()
+
+
+                
+    
