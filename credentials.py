82ae3283aa4b1e78642f53809890fb93bc7ba1ef
asandelarvine <larvine.asande@student.moringaschool.com>
2021-10-25 09:01:22 +0300

    delete and search methods implemented

--- credentials.py
+++ credentials.py
@@ -18,3 +18,48 @@ class Credentials:
         method saves credentials objects into the cred_list
         '''
         Credentials.cred_list.append(self)
+
+         ###delete credentials###
+    def delete_cred(self):
+        '''
+        method deletes credentials
+        '''
+        Credentials.cred_list.remove(self)
+
+        ###search for credentials###
+    @classmethod
+    def find_account(cls,cred_list):
+        '''
+        method searchs for accounts
+        '''
+        for cred in cls.cred_list:
+            if cred.account == account:
+                return cred
+
+             ###confirm credentials###
+    @classmethod
+    def cred_exists(cls,account):
+        '''
+        method confirms if a class exists
+        '''   
+        for cred in cls.cred_list:
+            if cred.account == account:
+                return True
+        return False
+
+        ###display credentials###
+    @classmethod
+    def display_cred(cls):
+        '''
+        method that returns all credentials
+        '''
+        return cls.cred_list
+
+
+        ###copy password###
+    @classmethod
+    def copy_passlock(cls, passlock):
+        find_account = Credentials.find_account(passlock)
+        pyperclip.copy(find_account.passlock)
+
+
